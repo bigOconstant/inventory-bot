001dc4bc0db413cfba1a27cf272f349c4e429ed5
caleb <caleb.a.mccarthy@gmail.com>
2021-01-06 21:43:30 -0600

    Best buy working

--- Readme.md
+++ Readme.md
@@ -1,3 +1,14 @@
 # GPU stock bot
 
-Tired of not being able to get a 6800xt, this bot will defeat all other slow python selenium bots.  
+Tired of not being able to get a 6800xt, this bot will defeat all other slow python selenium bots.
+
+# How to use
+Add items to urls section in settings.json. Three items are given as an example. Currently it just parses the page for a "add to cart" string.
+
+**item**: can be what ever name you want. It's just a name.
+
+**url**: is the url to check for the add to cart string. Works with bestbuy
+
+**delayseconds**: delay in seconds between checks. 
+
+Each url is checked in a seperate thread so they can be ran concurrently. 
--- main.go
+++ main.go
@@ -1,23 +1,72 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/corpix/uarand"
+	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
+/*
+Urls Holds item to pass to each worker
+*/
+type Urls struct {
+	Item string `json:"item"`
+	URL  string `json:"url"`
+}
+
+/*
+Settings holds settings json file
+*/
+type Settings struct {
+	Delayseconds int64  `json:"delayseconds"`
+	Useragent    string `json:"useragent"`
+	Urls         []Urls `json:"urls"`
+}
+
 func main() {
+	settingsFile, err := os.Open("settings.json")
+	settings := Settings{}
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	byteValue, _ := ioutil.ReadAll(settingsFile)
+	json.Unmarshal([]byte(byteValue), &settings)
+	settingsFile.Close()
+
+	for true {
+		var wg sync.WaitGroup
+		//fmt.Println("Spliting into threads")
+		for index, item := range settings.Urls {
+			wg.Add(index)
+			go checkStock(&wg, index, item, settings.Useragent)
+		}
+
+		//fmt.Println("Waiting for workers to finish")
+		wg.Wait()
+
+		//fmt.Println("pausing for ", settings.Delayseconds, " seconds")
+		time.Sleep(time.Duration(settings.Delayseconds) * time.Second)
+	}
+	fmt.Println("Main: Completed")
+}
+
+func checkStock(wg *sync.WaitGroup, id int, item Urls, Useragent string) {
+	defer wg.Done()
 	client := &http.Client{}
 
-	req, err := http.NewRequest("GET", "https://www.bestbuy.com/site/xfx-amd-radeon-rx-6800xt-16gb-gddr6-pci-express-4-0-gaming-graphics-card-black/6441226.p?skuId=6441226", nil)
+	req, err := http.NewRequest("GET", item.URL, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	req.Header.Set("User-Agent", uarand.GetRandom())
+	req.Header.Set("User-Agent", Useragent)
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -30,5 +79,10 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(string(body))
+	if strings.Contains(strings.ToUpper((string(body))), "ADD TO CART") {
+		fmt.Println(item.Item, " in stock go to ", item.URL, " now")
+
+	} else {
+		fmt.Println(item.Item, " not in stock")
+	}
 }
